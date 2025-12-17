818d1af343c72c4b3e05fffeb970ab794f702db7
sstasko <staskosebastian95@gmail.com>
2025-12-17 15:57:20 +0100

    api url fix'd

--- apps/web-app/app/api/api-client.ts
+++ apps/web-app/app/api/api-client.ts
@@ -1,7 +1,9 @@
 import { API } from "./generated-api";
 
+const API_VERSION = "v1";
+
 export const ApiClient = new API({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/${API_VERSION}` ,
   secure: true,
   withCredentials: true
 });
