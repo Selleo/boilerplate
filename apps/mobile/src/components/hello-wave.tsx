112339edc05b106616b7466d890c156ad8ec22b8
Tomasz Kielar <git@k1eu.dev>
2026-01-08 11:37:35 +0100

    feat(mobile): move to src folder

--- apps/mobile/src/components/hello-wave.tsx
+++ apps/mobile/src/components/hello-wave.tsx
@@ -3,9 +3,8 @@ import Animated from 'react-native-reanimated';
 export function HelloWave() {
   return (
     <Animated.Text
+    className="text-red-500"
       style={{
-        fontSize: 28,
-        lineHeight: 32,
         marginTop: -6,
         animationName: {
           '50%': { transform: [{ rotate: '25deg' }] },
@@ -13,7 +12,7 @@ export function HelloWave() {
         animationIterationCount: 4,
         animationDuration: '300ms',
       }}>
-      👋
+test
     </Animated.Text>
   );
 }
