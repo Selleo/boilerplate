8cfdab8b7a687109625862a1f34d4526f0853382
Tomasz Kielar <git@k1eu.dev>
2026-02-24 17:37:16 +0100

    chore: fix zod 4 import issues

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -1,8 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      zod: path.resolve(__dirname, "./node_modules/zod")
+    }
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
 });
