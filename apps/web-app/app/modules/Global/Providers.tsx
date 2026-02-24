6f625ee9b9c50865acb8fd8e095f9063b0ceb83f
Tomasz Kielar <git@k1eu.dev>
2026-02-24 11:29:30 +0100

    feat: update shadcn and radix components

--- apps/web-app/app/modules/Global/Providers.tsx
+++ apps/web-app/app/modules/Global/Providers.tsx
@@ -4,14 +4,17 @@ import { ThemeProvider } from "../Theme/ThemeProvider";
 import { queryClient } from "../../api/queryClient";
 import i18next from "~/lib/i18n";
 import { Toaster } from "~/components/ui/sonner";
+import { TooltipProvider } from "~/components/ui/tooltip";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <I18nextProvider i18n={i18next}>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider>
-          <Toaster />
-          {children}
+          <TooltipProvider>
+            <Toaster />
+            {children}
+          </TooltipProvider>
         </ThemeProvider>
       </QueryClientProvider>
     </I18nextProvider>
