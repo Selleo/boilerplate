6078641b396c9245855b1c74936d266f193ddc3c
Tomasz Kielar <git@k1eu.dev>
2026-05-05 16:45:48 +0200

    chore: adjust the config changes

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -1,20 +1,19 @@
 import { reactRouter } from "@react-router/dev/vite";
-  import tailwindcss from "@tailwindcss/vite";
-  import path from "path";
-  import { defineConfig } from "vite";
-  import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-  export default defineConfig({
-    resolve: {
-      alias: {
-        zod: path.resolve(__dirname, "./node_modules/zod")
-      }
-    },
-    server: {
-      host: "0.0.0.0",
-      port: 5000,
-      allowedHosts: true
-    },
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
-  });
-  
+export default defineConfig({
+  resolve: {
+    alias: {
+      zod: path.resolve(__dirname, "./node_modules/zod")
+    }
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: true
+  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
+});
